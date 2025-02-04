bea601f6d3f4adb1d3e3f87744d8c8f996dd1bfb
danielste555 <danielste555@gmail.com>
2025-02-04 12:44:59 -0500

    first push

---
+++ app/db/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from datetime import datetime
+import uuid
+
+class LoyaltyHistoryBase(BaseModel):
+    user_id: uuid.UUID
+    points: int
+    service: str
+    reference: str
+    amount: float
+    id_admin: uuid.UUID
+
+class LoyaltyHistoryCreate(LoyaltyHistoryBase):
+    pass
+
+class LoyaltyHistoryResponse(LoyaltyHistoryBase):
+    id: uuid.UUID
+    date_points: datetime
+
+    class Config:
+        orm_mode = True
